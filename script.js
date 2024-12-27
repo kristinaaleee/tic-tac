dfa22a44bfd6c9eda50def3f4b2522e859437c42
kristinaaleee <kristinalee2000@hotmail.com>
2024-12-26 22:45:06 -0500

    Create repo files and build in-console game struc
    Problem with adding symbol in right place

---
+++ script.js
@@ -0,0 +1,102 @@
+//gameboard object
+
+//player object
+
+//game flow object
+
+
+//win conditions array.every etc.
+
+function Gameboard(){
+    const board = new Array(3).fill(Square());
+    for (let i = 0; i < board.length; i++){
+        board[i] = new Array(3).fill(Square());
+    };
+    
+
+    const getBoard = () => board;
+
+    const printBoard = () =>{
+        const updateBoard = board.map((row) => row.map((square) => square.getValue()))
+        console.log(updateBoard);
+    };
+
+
+
+// currently filling whole row with same symbol
+
+    const markSquare = (row, column, player) =>{
+        rowIndex = row - 1;
+        colIndex = column - 1;
+
+        // console.log(board[rowIndex][colIndex])
+        // if (board[rowIndex][colIndex] != undefined) return;
+
+        board[rowIndex][colIndex].addSymbol(player);
+    };
+
+    return{getBoard, markSquare, printBoard};
+};
+
+function Square(){
+    let value = "";
+
+    const addSymbol = (player) =>{
+        value = player;
+    }
+    const getValue = () => value;
+
+    return {addSymbol, getValue};
+}
+
+function GameController(){
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: 'Player 1',
+            symbol: 'X'
+        },
+        {
+            name: 'Player 2',
+            symbol: 'O'
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () =>{
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn. `);
+    };
+
+    const playRound = (row, column) =>{
+        console.log(`${getActivePlayer().name} move into row ${row}, column ${column}.`);
+
+        board.markSquare(row, column, getActivePlayer().symbol);
+
+        switchPlayerTurn();
+        printNewRound();
+        };
+
+        printNewRound();
+
+    return{
+        playRound, 
+        getActivePlayer
+    };
+};
+
+const game = GameController();
+
+
+
+
+
+
